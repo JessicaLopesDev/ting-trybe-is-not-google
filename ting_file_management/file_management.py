1716fb11b7f1fa83b06afa934a98ac8dfe907a9b
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-08-28 18:33:31 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        return print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, "r") as file:
+            return file.read().split('\n')
+
+    except FileNotFoundError:
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+# Usando duas formas de exibir mensagem na stderr para fins de pesquisa futura
