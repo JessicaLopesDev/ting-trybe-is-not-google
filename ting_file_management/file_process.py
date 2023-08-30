f06151e7f0256439abf13cd803c5e887e6cb51fa
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-08-30 16:48:09 -0300

    Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_text = txt_importer(path_file)
+
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_text),
+        "linhas_do_arquivo": file_text,
+    }
+
+    if instance.contain(path_file):
+        return
+
+    instance.enqueue(file_data)
+    sys.stdout.write(f"{file_data}\n")
 
 
 def remove(instance):
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -8,13 +8,18 @@ class Queue(AbstractQueue):
     def __len__(self):
         return len(self.__data)
 
+    def contain(self, value):
+        return any(file["nome_do_arquivo"] == value for file in self.__data)
+
     def enqueue(self, value):
         self.__data.append(value)
 
     def dequeue(self):
+        if len(self.__data) == 0:
+            raise IndexError("Empty")
         return self.__data.pop(0)
 
     def search(self, index):
-        if index < 0 or index >= len(self.__data):
+        if index not in range(len(self.__data)):
             raise IndexError("Índice Inválido ou Inexistente")
         return self.__data[index]
