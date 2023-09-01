8d5a2c3b633478f8aee9247722521abbe7b80985
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-01 15:39:36 -0300

    Requisitos 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word = word.lower()
+    occurrences = []
+
+    for index in range(len(instance)):
+        data = instance.search(index)
+        result = {
+            "palavra": word,
+            "arquivo": data["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for line_number, line in enumerate(data["linhas_do_arquivo"], start=1):
+            if word in line.lower():
+                result["ocorrencias"].append({"linha": line_number})
+
+        if result["ocorrencias"]:
+            occurrences.append(result)
+
+    return occurrences
 
 
 def search_by_word(word, instance):
