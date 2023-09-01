320d9634b82063a66f9215900946a50f9f2156a7
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-01 15:45:59 -0300

    Requisito 8 - utilizando req 7 sem refatorar

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -21,4 +21,22 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    word = word.lower()
+    occurrences = []
+
+    for index in range(len(instance)):
+        data = instance.search(index)
+        result = {
+            "palavra": word,
+            "arquivo": data["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+
+        for line_number, line in enumerate(data["linhas_do_arquivo"], start=1):
+            if word in line.lower():
+                result["ocorrencias"].append({"linha": line_number, "conteudo": line})
+
+        if result["ocorrencias"]:
+            occurrences.append(result)
+
+    return occurrences
