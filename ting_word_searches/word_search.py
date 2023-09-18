eb0903a0453b56f5ed160f9264df42d50da098b6
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-09-18 17:48:17 -0300

    fix

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -24,19 +24,8 @@ def search_by_word(word, instance):
     word = word.lower()
     occurrences = []
 
-    for index in range(len(instance)):
-        data = instance.search(index)
-        result = {
-            "palavra": word,
-            "arquivo": data["nome_do_arquivo"],
-            "ocorrencias": []
-        }
-
-        for line_number, line in enumerate(data["linhas_do_arquivo"], start=1):
-            if word in line.lower():
-                result["ocorrencias"].append({"linha": line_number, "conteudo": line})
-
-        if result["ocorrencias"]:
-            occurrences.append(result)
+    for line_number, line in enumerate(instance["linhas_do_arquivo"], start=1):
+        if word in line.lower():
+            occurrences.append({"linha": line_number, "conteudo": line})
 
     return occurrences
